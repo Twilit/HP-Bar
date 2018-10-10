bf1957c0f27b99eb2b07de396c97046d5512dd44
Twilit <kinlongsha@hotmail.co.uk>
2018-10-10 11:10:38 +0100

    Adding SCripts

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField]
+    private Stat health;
+
+    private void Awake()
+    {
+        health.Initialize();
+    }
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.Q))
+        {
+            health.CurrentVal -= 10;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            health.CurrentVal += 10;
+        }
+	}
+}
